5bebf43fe91def78f681103b0dc41abcb315cfd9
SamsonJunior <juliussy46ij@nsuk.edu.ng>
2025-09-27 22:36:34 +0100

    Fix ALLOWED_HOSTS properly

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY","fallback-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False") == "True"
 
-ALLOWED_HOSTS = ['my-portfolio-projects-1.onrender.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['my-portfolio-projects-1.onrender.com']
 
 
 # Application definition
